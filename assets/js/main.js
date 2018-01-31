64416be0f07f75bcaa9a567f2da26966da1e01cb
Caleb Cox <caleb@caleb-cox.com>
2018-01-30 22:17:58 -0500

    Resolved fetch issue for wiki JSON data

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,37 @@ const randBtn = document.querySelector("#random");
 const searchField = document.querySelector(".searchField");
 
 searchBtn.addEventListener("click", () => {
+    console.log('clicked');
     let query = searchField.value;
-    let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + query + "&format=json&callback=?";
+    let url = "https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=" + query + "&format=json";
+    console.log('URL: ' + url);
     fetch(url)
     .then(handleErrors)
     .then(parseJSON)
-    .catch((error) => {
-        console.log(error)
-    })
+    .catch(printError)
     // log results
 })
 
 function handleErrors(res) {
     if (!res.ok) {
+        console.log(res);
         throw Error(res.status);
     }
     return res;
 }
 
 function parseJSON(res) {
+    console.log('parseJSON Response: ' + res)
     return res.json().then((parsedData) => {
         // return parsedData;
         console.log(parsedData);
     });
 };
+
+// function parseData(res) {
+//     console.log(res);
+// }
+
+function printError(error) {
+    console.log(error);
+}
